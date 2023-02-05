6d0effb0245482e271544422bb9defa8364f22ce
Simon Biickert <simon@biickert.ca>
2023-02-05 09:24:55 -0700

    Day 10 part 2 complete in objc

--- ObjC/AoC2017/AoC2017/AOCDay10.m
+++ ObjC/AoC2017/AoC2017/AOCDay10.m
@@ -5,6 +5,7 @@
 
 #import <Foundation/Foundation.h>
 #import "AOCDay10.h"
+#import "AOCStrings.h"
 
 @implementation AOCDay10
 
@@ -18,37 +19,84 @@
 	
 	NSArray<NSString *> *input = [AOCInput readGroupedInputFile:filename atIndex:index];
 	
-	NSArray<NSNumber *> *lengths = [[input[0] componentsSeparatedByString:@", "] valueForKey:@"integerValue"];
 	
-	result.part1 = [self solvePartOne: lengths];
-	result.part2 = [self solvePartTwo: input];
+	result.part1 = [self solvePartOne: input[0]];
+	
+	// Tests
+//	[[self solvePartTwo:@""] println];
+//	[[self solvePartTwo:@"AoC 2017"] println];
+//	[[self solvePartTwo:@"1,2,3"] println];
+//	[[self solvePartTwo:@"1,2,4"] println];
+
+	result.part2 = [self solvePartTwo: input[0]];
 	
 	return result;
 }
 
-- (NSString *)solvePartOne:(NSArray<NSNumber *> *)lengths {
-	NSInteger skipSize = 0;
-	NSInteger position = 0;
+- (NSString *)solvePartOne:(NSString *)input {
+	NSArray<NSNumber *> *lengths = [[input componentsSeparatedByString:@","] valueForKey:@"integerValue"];
 	NSMutableArray<NSNumber *> *list = [NSMutableArray array];
-	
 	for (int i = 0; i < ((lengths.count == 4) ? 5 : 256); i++) {
 		[list addObject:@(i)];
 	}
 	
-	for (NSNumber *length in lengths) {
-		NSInteger end = position + (length.integerValue - 1);
-		[self reverseNumbersIn:list startIndex:position endIndex:end];
-		position = (position + length.integerValue + skipSize) % list.count;
-		skipSize++;
-		//NSLog(@"%@", list);
-	}
+	NSInteger skipSize = 0;
+	NSInteger position = 0;
+	
+	[self knotHashIn:list lengths:lengths position:&position skipSize:&skipSize];
 	
 	NSInteger productOfFirstTwo = list[0].intValue * list[1].intValue; // 10920 too high
 	
 	return [NSString stringWithFormat:@"%ld", productOfFirstTwo];
 }
 
-- (void)reverseNumbersIn:(NSMutableArray<NSNumber *> *)array startIndex:(NSInteger)start endIndex:(NSInteger)end {
+
+- (NSString *)solvePartTwo:(NSString *)input {
+	NSMutableArray<NSNumber *> *lengths = [NSMutableArray array];
+	for (int i = 0; i < input.length; i++) {
+		[lengths addObject: @([input characterAtIndex:i])];
+	}
+	[lengths addObjectsFromArray:@[@17, @31, @73, @47, @23]];
+	
+	NSMutableArray<NSNumber *> *sparseHash = [NSMutableArray array];
+	for (int i = 0; i < ((lengths.count == 4) ? 5 : 256); i++) {
+		[sparseHash addObject:@(i)];
+	}
+	
+	NSInteger skipSize = 0;
+	NSInteger position = 0;
+	
+	for (int i = 0; i < 64; i++) {
+		[self knotHashIn:sparseHash lengths:lengths position:&position skipSize:&skipSize];
+	}
+	
+	NSArray<NSNumber *> *denseHash = [self denseHashFrom:sparseHash];
+	NSMutableString *hexString = [NSMutableString string];
+	
+	for (NSNumber *number in denseHash) {
+		[hexString appendFormat:@"%02x", number.intValue];
+	}
+		
+	return hexString;
+}
+
+
+- (void)knotHashIn:(NSMutableArray<NSNumber *> *)array
+		   lengths:(NSArray<NSNumber *> *)lengths
+		  position:(NSInteger *)position
+		  skipSize:(NSInteger *)skipSize
+{
+	for (NSNumber *length in lengths) {
+		NSInteger end = *position + (length.integerValue - 1);
+		[self reverseNumbersIn:array startIndex:*position endIndex:end];
+		*position = (*position + length.integerValue + *skipSize) % array.count;
+		*skipSize = *skipSize + 1;
+		//NSLog(@"%@", list);
+	}
+}
+
+- (void)reverseNumbersIn:(NSMutableArray<NSNumber *> *)array startIndex:(NSInteger)start endIndex:(NSInteger)end
+{
 	NSInteger length = end - start;
 	end = end % array.count;
 	NSInteger a = start;
@@ -63,9 +111,19 @@
 	}
 }
 
-- (NSString *)solvePartTwo:(NSArray<NSString *> *)input {
+- (NSArray<NSNumber *> *)denseHashFrom:(NSArray<NSNumber *> *)sparseHash
+{
+	NSMutableArray *result = [NSMutableArray array];
 	
-	return @"World";
+	for (int block = 0; block < 16; block++) {
+		NSInteger number = sparseHash[block*16].integerValue;
+		for (int element = 1; element < 16; element++) {
+			number = number ^ sparseHash[block*16+element].integerValue;
+		}
+		[result addObject:@(number)];
+	}
+	
+	return result;
 }
 
 @end
