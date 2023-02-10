b76759885dc3b04f9aa44f6a5db0b7a09bc0216a
Simon Biickert <simon@biickert.ca>
2023-02-09 21:46:25 -0700

    Day 13 part 2 complete in objc

--- ObjC/AoC2017/AoC2017/AOCDay13.m
+++ ObjC/AoC2017/AoC2017/AOCDay13.m
@@ -21,32 +21,50 @@
 	NSDictionary<NSNumber *, NSNumber *> *scanners = [self parseInput:input];
 	
 	result.part1 = [self solvePartOne: scanners];
-	result.part2 = [self solvePartTwo: input];
+	result.part2 = [self solvePartTwo: scanners];
 	
 	return result;
 }
 
 - (NSString *)solvePartOne:(NSDictionary<NSNumber *, NSNumber *> *)scanners {
+	NSInteger totalSeverity = [self sendPacket:scanners withDelay:0];
+	return [NSString stringWithFormat:@"%ld", totalSeverity];
+}
+
+- (NSString *)solvePartTwo:(NSDictionary<NSNumber *, NSNumber *> *)scanners {
+	NSInteger totalSeverity = 1;
+	NSInteger delay = -1;
+	
+	while (totalSeverity > 0) {
+		delay++;
+		totalSeverity = [self sendPacket:scanners withDelay:delay];
+		if (delay % 100000 == 0) {
+			NSLog(@"%ld",  delay);
+		}
+	}
+	
+	return [NSString stringWithFormat:@"%ld", delay];
+}
+
+- (NSInteger)sendPacket:(NSDictionary<NSNumber *, NSNumber *> *)scanners
+			  withDelay:(NSInteger)delay
+{
 	NSInteger totalSeverity = 0;
+	
 	for (NSNumber *key in scanners.allKeys) {
-		NSInteger depth = key.integerValue;
+		NSInteger depth = key.integerValue + delay;
 		NSInteger range = scanners[key].integerValue;
-		// 2 -> 2
-		// 3 -> 4
-		// 4 -> 6
-		// 5 -> 8
+		// 2 -> 2, 3 -> 4, 4 -> 6, 5 -> 8
 		NSInteger atZeroEvery = (range - 1) * 2;
 		if (depth % atZeroEvery == 0) {
 			NSInteger severity = depth * range;
 			totalSeverity += severity;
 		}
+		if (delay > 0 && totalSeverity > 0) {
+			break;
+		}
 	}
-	return [NSString stringWithFormat:@"%ld", totalSeverity];
-}
-
-- (NSString *)solvePartTwo:(NSArray<NSString *> *)input {
-	
-	return @"World";
+	return totalSeverity;
 }
 
 - (NSDictionary<NSNumber *, NSNumber *> *)parseInput:(NSArray<NSString *> *)input
