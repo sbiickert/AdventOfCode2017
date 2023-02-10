683b8e8f286a90458930c8278b3feef792ba60f0
Simon Biickert <simon@biickert.ca>
2023-02-09 20:23:13 -0700

    Day 13 part 1 complete in objc

--- ObjC/AoC2017/AoC2017/AOCDay13.m
+++ ObjC/AoC2017/AoC2017/AOCDay13.m
@@ -18,20 +18,47 @@
 	
 	NSArray<NSString *> *input = [AOCInput readGroupedInputFile:filename atIndex:index];
 	
-	result.part1 = [self solvePartOne: input];
+	NSDictionary<NSNumber *, NSNumber *> *scanners = [self parseInput:input];
+	
+	result.part1 = [self solvePartOne: scanners];
 	result.part2 = [self solvePartTwo: input];
 	
 	return result;
 }
 
-- (NSString *)solvePartOne:(NSArray<NSString *> *)input {
-	
-	return @"Hello";
+- (NSString *)solvePartOne:(NSDictionary<NSNumber *, NSNumber *> *)scanners {
+	NSInteger totalSeverity = 0;
+	for (NSNumber *key in scanners.allKeys) {
+		NSInteger depth = key.integerValue;
+		NSInteger range = scanners[key].integerValue;
+		// 2 -> 2
+		// 3 -> 4
+		// 4 -> 6
+		// 5 -> 8
+		NSInteger atZeroEvery = (range - 1) * 2;
+		if (depth % atZeroEvery == 0) {
+			NSInteger severity = depth * range;
+			totalSeverity += severity;
+		}
+	}
+	return [NSString stringWithFormat:@"%ld", totalSeverity];
 }
 
 - (NSString *)solvePartTwo:(NSArray<NSString *> *)input {
 	
 	return @"World";
+}
+
+- (NSDictionary<NSNumber *, NSNumber *> *)parseInput:(NSArray<NSString *> *)input
+{
+	NSMutableDictionary<NSNumber *, NSNumber *> *result = [NSMutableDictionary dictionary];
+	
+	for (NSString *line in input) {
+		NSArray<NSString *> *parts = [line componentsSeparatedByString:@": "];
+		[result setObject: @(parts[1].integerValue) forKey:@(parts[0].integerValue)];
+	}
+	
+	return result;
 }
 
 @end
