b997cbf8717a3e0127b95fc03437d2804d4a1dd1
Simon Biickert <simon@biickert.ca>
2023-02-13 07:33:52 -0700

    Day 16 some optimizations

--- ObjC/AoC2017/AoC2017/AOCDay16.m
+++ ObjC/AoC2017/AoC2017/AOCDay16.m
@@ -24,49 +24,70 @@
 									  @"i",@"j",@"k",@"l",
 									  @"m",@"n",@"o",@"p",];
 
-	NSArray<NSString *> *moves = [input[0] componentsSeparatedByString:@","];
+	NSArray<NSDictionary *> *moves = [self parseMoves:input[0]];
 	
 	result.part1 = [self solvePartOne: programs moves:moves];
-	result.part2 = [self solvePartTwo: input];
+	result.part2 = [self solvePartTwo: programs moves:moves];
 	
 	return result;
 }
 
-- (NSString *)solvePartOne:(NSArray<NSString *> *)immutablePrograms moves:(NSArray<NSString *> *)moves {
-	NSMutableArray<NSString *> *programs = [immutablePrograms mutableCopy];
-	for (NSString *move in moves) {
-		NSString *indexInfo = [move substringFromIndex:1];
-		if ([move hasPrefix:@"s"]) {
-			// spin: e.g. "s4"
-			int size = [indexInfo intValue];
-			[self spin:programs size:size];
-		}
-		else if ([move hasPrefix:@"x"]) {
-			// exchange: e.g. "x5/6"
-			NSArray<NSNumber *> *indexes = [[indexInfo componentsSeparatedByString:@"/"] valueForKey:@"integerValue"];
-			[self exchange:programs position:indexes[0].intValue position:indexes[1].intValue];
-		}
-		else {
-			// partner: e.g. "ph/o
-			NSArray<NSString *> *names = [indexInfo componentsSeparatedByString:@"/"];
-			[self partner:programs name:names[0] name:names[1]];
-		}
-	}
-	return [programs componentsJoinedByString:@""];
+- (NSString *)solvePartOne:(NSArray<NSString *> *)programs moves:(NSArray<NSDictionary *> *)moves {
+	return [self dance:programs moves:moves iterations:1];
 }
 
-- (NSString *)solvePartTwo:(NSArray<NSString *> *)input {
+- (NSString *)solvePartTwo:(NSArray<NSString *> *)programs moves:(NSArray<NSDictionary *> *)moves {
+//	return [self dance:programs moves:moves iterations:100];
+	return [self dance:programs moves:moves iterations:1000000000]; // 1 billion
+}
+
+- (NSString *)dance:(NSArray<NSString *> *)immutablePrograms moves:(NSArray<NSDictionary *> *)moves iterations:(int)count
+{
+	NSMutableArray<NSString *> *programs = [immutablePrograms mutableCopy];
+	//NSMutableDictionary<NSString *, NSNumber *> *history = [NSMutableDictionary dictionary];
+	NSString *joined;
 	
-	return @"World";
+	for (int i = 0; i < count; i++) {
+		for (NSDictionary *move in moves) {
+			if ([move[@"type"] isEqualToString:@"spin"]) {
+				[self spin:programs size:[move[@"size"] intValue]];
+			}
+			else if ([move[@"type"] isEqualToString:@"exchange"]) {
+				[self exchange:programs position:[move[@"p1"] intValue] position:[move[@"p2"] intValue]];
+			}
+			else {
+				[self partner:programs name:move[@"n1"] name:move[@"n2"]];
+			}
+		}
+		
+		joined = [programs componentsJoinedByString:@""];
+		
+//		if ([history objectForKey:joined] != nil) {
+//			NSLog(@"%@ repeated. First seen at %@, now at %d", joined, [history objectForKey:joined], i);
+//		}
+		if (i % 1000 == 0) {
+			NSLog(@"%d", i);
+		}
+	}
+	return joined;
 }
 
 - (void)spin:(NSMutableArray<NSString *> *)programs size:(int)size
 {
-	for (int i = 0; i < size; i++) {
-		NSString *program = [programs lastObject];
-		[programs removeLastObject];
-		[programs insertObject:program atIndex:0];
-	}
+	NSRange r = NSMakeRange(programs.count-size, size);
+	NSIndexSet *iset = [NSIndexSet indexSetWithIndexesInRange:r];
+	id temp = [programs objectsAtIndexes:iset];
+	[programs removeObjectsAtIndexes:iset];
+	
+	r = NSMakeRange(0, size);
+	iset = [NSIndexSet indexSetWithIndexesInRange:r];
+	[programs insertObjects:temp atIndexes:iset];
+	
+//	for (int i = 0; i < size; i++) {
+//		NSString *program = [programs lastObject];
+//		[programs removeLastObject];
+//		[programs insertObject:program atIndex:0];
+//	}
 }
 
 - (void)exchange:(NSMutableArray<NSString *> *)programs position:(int)p1 position:(int)p2
@@ -81,6 +102,33 @@
 	int p1 = (int)[programs indexOfObject:n1];
 	int p2 = (int)[programs indexOfObject:n2];
 	[self exchange:programs position:p1 position:p2];
+}
+
+- (NSArray<NSDictionary *> *)parseMoves:(NSString *)input
+{
+	NSArray<NSString *> *movesDefinitions = [input componentsSeparatedByString:@","];
+	NSMutableArray<NSDictionary *> *moves = [NSMutableArray array];
+	
+	for (NSString *defn in movesDefinitions) {
+		NSString *indexInfo = [defn substringFromIndex:1];
+		if ([defn hasPrefix:@"s"]) {
+			// spin: e.g. "s4"
+			int size = [indexInfo intValue];
+			[moves addObject:@{ @"type": @"spin", @"size": @(size)}];
+		}
+		else if ([defn hasPrefix:@"x"]) {
+			// exchange: e.g. "x5/6"
+			NSArray<NSNumber *> *indexes = [[indexInfo componentsSeparatedByString:@"/"] valueForKey:@"integerValue"];
+			[moves addObject:@{ @"type": @"exchange", @"p1": indexes[0], @"p2": indexes[1]}];
+		}
+		else {
+			// partner: e.g. "ph/o
+			NSArray<NSString *> *names = [indexInfo componentsSeparatedByString:@"/"];
+			[moves addObject:@{ @"type": @"partner", @"n1": names[0], @"n2": names[1]}];
+		}
+	}
+	
+	return moves;
 }
 
 @end
