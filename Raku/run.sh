4aeed0041d33ab1ab8be479608453c52003c3cd8
Simon Biickert <simon@biickert.ca>
2025-07-29 16:13:50 -0600

    Create run.sh

---
+++ Raku/run.sh
@@ -0,0 +1,27 @@
+#! /bin/sh
+
+./day01.raku
+./day02.raku
+./day03.raku
+./day04.raku
+./day05.raku
+./day06.raku
+./day07.raku
+./day08.raku
+./day09.raku
+./day10.raku
+./day11.raku
+./day12.raku
+./day13.raku
+./day14.raku
+./day15.raku
+./day16.raku
+./day17.raku
+./day18.raku
+./day19.raku
+./day20.raku
+./day21.raku
+./day22.raku
+./day23.raku
+./day24.raku
+./day25.raku
