ea304020ebc7a4852208ce347c30d00a677713ce
Simon Biickert <simon@biickert.ca>
2025-08-01 08:32:35 -0600

    Day 4 complete in F#

--- F#/AoC2017/Day04.fs
+++ F#/AoC2017/Day04.fs
@@ -8,11 +8,18 @@ let passphraseIsValidPt1 (passphrase:string): bool =
     let uniqueWords = words |> Set.ofArray
     uniqueWords.Count = words.Length
 
-let passphraseIsValidPt2 passphrase: bool =
-    true
+let sortChars (word:string):string =
+    let chars = word.ToCharArray() |> Array.sort
+    new string(chars)
 
-let solvePartOne input =
-    input |> List.filter passphraseIsValidPt1 |> List.length
+let areAnagrams (word1:string, word2:string): bool =
+    word1.Length = word2.Length && sortChars word1 = sortChars word2
+
+let passphraseIsValidPt2 passphrase: bool =
+    passphraseIsValidPt1 passphrase &&
+        let words = passphrase.Split(" ")
+        let anagramCount = AoC.Util.combinations words |> Seq.filter areAnagrams |> Seq.length
+        anagramCount = 0
 
 let solvePart validator input =
     input |> List.filter validator |> List.length
