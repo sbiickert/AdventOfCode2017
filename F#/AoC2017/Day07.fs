785a3b931f7363850f3b421fe1338b4aa6615cc9
Simon Biickert <simon@biickert.ca>
2025-08-11 16:50:36 -0600

    Day 7 part 1 complete in F#
    A bit of part 2, too.

--- F#/AoC2017/Day07.fs
+++ F#/AoC2017/Day07.fs
@@ -2,12 +2,44 @@
 module Day07
 
 open AoC.Util
+open System.Text.RegularExpressions
 
-let solvePartOne input =
-    1
+type WeightAndChildren = 
+    {
+        weight: int
+        children: List<string>
+    }
 
-let solvePartTwo input =
-    2
+let rec totalWeight (program:string) (map:Map<string,WeightAndChildren>): int =
+    let wc = map[program]
+    let childWeight = wc.children |> List.fold(fun acc elem -> acc + totalWeight elem map) 0
+    wc.weight + childWeight
+
+let solvePartOne (map:Map<string,WeightAndChildren>): string =
+    let allPrograms = map.Keys |> Set.ofSeq
+    let allChildren =
+        map.Values |> Seq.map(fun wc -> wc.children) |> Seq.collect id |> Set.ofSeq
+    allChildren |> Set.difference allPrograms |> Set.toList |> List.head
+
+let solvePartTwo (map:Map<string,WeightAndChildren>): (string * int) =
+    
+    "bob",2
+
+let buildMap input =
+    let mutable map = Map<string,WeightAndChildren> []
+    let rx = Regex(@"(\w+) \((\d+)\)( -> (.+))?")
+    input |> List.map (fun (line:string) ->
+        let m = rx.Match(line).Groups
+        let name = m[1].Value
+        let weight = int m[2].Value
+        let children =
+            if m[3].Success then
+                m[4].Value.Split(", ") |> List.ofArray
+            else
+                []
+        map <- map.Add (name, {weight=weight; children=children})
+    ) |> ignore
+    map
 
 let solveDay07 isTest: Unit =
     let day = 07
@@ -16,7 +48,9 @@ let solveDay07 isTest: Unit =
     let inputName = inputFileName day isTest
     let input = readInput inputName true
 
-    let solution1 = solvePartOne input
-    printfn $"Part One: {solution1}"
-    let solution2 = solvePartTwo input
-    printfn $"Part Two: {solution2}"
+    let map = buildMap input
+
+    let solution1 = solvePartOne map
+    printfn $"Part One: the program at the bottom is {solution1}"
+    let solution2Name,solution2Weight = solvePartTwo map
+    printfn $"Part Two: the program {solution2Name} should weigh {solution2Weight}"
--- F#/AoC2017/worksheet.fsx
+++ F#/AoC2017/worksheet.fsx
@@ -1,9 +1,11 @@
-#load "AoCUtil.fs"
+// #load "AoCUtil.fs"
 // #load "AoCGeometry.fs"
 // #load "AoCGrid.fs"
 
-#load "Day06.fs"
-#time
-solveDay06 false |> ignore
-#time
+// #load "Day06.fs"
+// #time
+// solveDay06 false |> ignore
+// #time
 
+let l = [1;2;3]
+l |> List.reduce (fun acc elem -> acc + elem)
